92450ac10d03c1c2119531be0e6e42da20c21d71
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-07-29 17:13:53 -0400

    Add ios 12 and above support

--- Controllers/BackgroundModeSettingsViewController.m
+++ Controllers/BackgroundModeSettingsViewController.m
@@ -2,6 +2,12 @@
 #import "../Headers/TheosLinuxFix.h"
 #import "../Headers/iOS15Fix.h"
 
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+
 @interface BackgroundModeSettingsViewController ()
 @end
 
@@ -37,7 +43,11 @@ int selectedIndex;
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     if (section == 0) {
-        return 3;
+        if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"15.0")) {
+            return 3;
+        } else {
+            return 2;
+        }
     }
     return 0;
 }
