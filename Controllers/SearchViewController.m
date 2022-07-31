73a299ea798e40209a5fd26a70ffbbaf0cf00280
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-07-30 21:07:10 -0400

    Search controller redesign

--- Controllers/SearchViewController.m
+++ Controllers/SearchViewController.m
@@ -55,7 +55,7 @@
 	for (int i = 1 ; i <= 50 ; i++) {
 		@try {
 			UIView *searchView = [[UIView alloc] init];
-			searchView.frame = CGRectMake(0, viewBounds, self.view.bounds.size.width, 80);
+			searchView.frame = CGRectMake(0, viewBounds, self.view.bounds.size.width, 100);
 			searchView.backgroundColor = [UIColor colorWithRed:0.110 green:0.110 blue:0.118 alpha:1.0];
 			searchView.tag = i;
 			UITapGestureRecognizer *searchViewTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(searchTap:)];
@@ -63,23 +63,46 @@
 			[searchView addGestureRecognizer:searchViewTap];
 
 			UIImageView *videoImage = [[UIImageView alloc] init];
-			videoImage.frame = CGRectMake(0, 0, 80, searchView.frame.size.height);
+			videoImage.frame = CGRectMake(0, 0, 80, 80);
             NSArray *videoArtworkArray = searchContents[i][@"compactVideoRenderer"][@"thumbnail"][@"thumbnails"];
             NSURL *videoArtwork = [NSURL URLWithString:[NSString stringWithFormat:@"%@", videoArtworkArray[([videoArtworkArray count] - 1)][@"url"]]];
 			videoImage.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:videoArtwork]];
 			[searchView addSubview:videoImage];
 
+            UILabel *videoTimeLabel = [[UILabel alloc] init];
+			videoTimeLabel.frame = CGRectMake(40, 65, 40, 15);
+			videoTimeLabel.text = [NSString stringWithFormat:@"%@", searchContents[i][@"compactVideoRenderer"][@"lengthText"][@"runs"][0][@"text"]];
+            videoTimeLabel.textAlignment = UITextAlignmentCenter;
+			videoTimeLabel.textColor = [UIColor whiteColor];
+			videoTimeLabel.numberOfLines = 1;
+            videoTimeLabel.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.4];
+            videoTimeLabel.layer.cornerRadius = 5;
+            videoTimeLabel.clipsToBounds = YES;
+			videoTimeLabel.adjustsFontSizeToFitWidth = true;
+			videoTimeLabel.adjustsFontForContentSizeCategory = false;
+			[searchView addSubview:videoTimeLabel];
+
 			UILabel *videoTitleLabel = [[UILabel alloc] init];
-			videoTitleLabel.frame = CGRectMake(85, 0, searchView.frame.size.width - 85, searchView.frame.size.height);
+			videoTitleLabel.frame = CGRectMake(85, 0, searchView.frame.size.width - 85, 80);
 			videoTitleLabel.text = [NSString stringWithFormat:@"%@", searchContents[i][@"compactVideoRenderer"][@"title"][@"runs"][0][@"text"]];
 			videoTitleLabel.textColor = [UIColor whiteColor];
 			videoTitleLabel.numberOfLines = 2;
 			videoTitleLabel.adjustsFontSizeToFitWidth = true;
 			videoTitleLabel.adjustsFontForContentSizeCategory = false;
 			[searchView addSubview:videoTitleLabel];
+
+            UILabel *videoCountAndAuthorLabel = [[UILabel alloc] init];
+			videoCountAndAuthorLabel.frame = CGRectMake(5, 80, searchView.frame.size.width - 5, 20);
+			videoCountAndAuthorLabel.text = [NSString stringWithFormat:@"%@ - %@", searchContents[i][@"compactVideoRenderer"][@"viewCountText"][@"runs"][0][@"text"], searchContents[i][@"compactVideoRenderer"][@"longBylineText"][@"runs"][0][@"text"]];
+			videoCountAndAuthorLabel.textColor = [UIColor whiteColor];
+			videoCountAndAuthorLabel.numberOfLines = 1;
+            [videoCountAndAuthorLabel setFont:[UIFont systemFontOfSize:12]];
+			videoCountAndAuthorLabel.adjustsFontSizeToFitWidth = true;
+			videoCountAndAuthorLabel.adjustsFontForContentSizeCategory = false;
+			[searchView addSubview:videoCountAndAuthorLabel];
 			
 			[searchVideoIDDictionary setValue:[NSString stringWithFormat:@"%@", searchContents[i][@"compactVideoRenderer"][@"videoId"]] forKey:[NSString stringWithFormat:@"%d", i]];
-			viewBounds += 82;
+			viewBounds += 102;
 
 			[scrollView addSubview:searchView];
 		}
