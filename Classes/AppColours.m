632ff30cd9b5d78a4ece79b716e08d15116364bd
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-08-05 17:13:57 -0400

    Add device theme support

--- Classes/AppColours.m
+++ Classes/AppColours.m
@@ -3,28 +3,46 @@
 @implementation AppColours
 
 + (UIColor *)mainBackgroundColour {
-    if ([[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 1 || [[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 2) {
+    if ([[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 1) {
+        return [UIColor colorWithRed:0.949 green:0.949 blue:0.969 alpha:1.0];
+    } else if ([[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 2 || [[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 3) {
         return [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
     } else {
-        return [UIColor colorWithRed:0.949 green:0.949 blue:0.969 alpha:1.0];
+        if (UITraitCollection.currentTraitCollection.userInterfaceStyle == UIUserInterfaceStyleLight) {
+            return [UIColor colorWithRed:0.949 green:0.949 blue:0.969 alpha:1.0];
+        } else {
+            return [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
+        }
     }
 }
 
 + (UIColor *)viewBackgroundColour {
     if ([[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 1) {
-        return [UIColor colorWithRed:0.110 green:0.110 blue:0.118 alpha:1.0];
+        return [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
     } else if ([[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 2) {
+        return [UIColor colorWithRed:0.110 green:0.110 blue:0.118 alpha:1.0];
+    } else if ([[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 3) {
         return [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:1.0];
     } else {
-        return [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
+        if (UITraitCollection.currentTraitCollection.userInterfaceStyle == UIUserInterfaceStyleLight) {
+            return [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
+        } else {
+            return [UIColor colorWithRed:0.110 green:0.110 blue:0.118 alpha:1.0];
+        }
     }
 }
 
 + (UIColor *)textColour {
-    if ([[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 1 || [[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 2) {
+    if ([[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 1) {
+        return [UIColor blackColor];
+    } else if ([[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 2 || [[NSUserDefaults standardUserDefaults] integerForKey:@"kAppTheme"] == 3) {
         return [UIColor whiteColor];
     } else {
-        return [UIColor blackColor];
+        if (UITraitCollection.currentTraitCollection.userInterfaceStyle == UIUserInterfaceStyleLight) {
+            return [UIColor blackColor];
+        } else {
+            return [UIColor whiteColor];
+        }
     }
 }
 
