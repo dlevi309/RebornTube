98b0af249125b6667d978b6b81cc5829153de6c7
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-08-11 18:32:40 -0400

    Add add to playlist option to youtube loader, cleanup youtube loader

--- Classes/YouTubeLoader.m
+++ Classes/YouTubeLoader.m
@@ -1,6 +1,7 @@
 #import "YouTubeLoader.h"
 #import "YouTubeExtractor.h"
 #import "../Controllers/PlayerViewController.h"
+#import "../Controllers/Playlists/AddToPlaylistsViewController.h"
 
 @implementation YouTubeLoader
 
@@ -66,103 +67,67 @@
         audioURL = audioLow;
     }
 
+    UIAlertController *alertQualitySelector;
+
     if ([UIDevice currentDevice].userInterfaceIdiom != UIUserInterfaceIdiomPad) {
-        UIAlertController *alertQualitySelector = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
-    
-        if (videoStream != nil) {
-            [alertQualitySelector addAction:[UIAlertAction actionWithTitle:@"Stream" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-                PlayerViewController *playerViewController = [[PlayerViewController alloc] init];
-                playerViewController.videoID = videoID;
-                playerViewController.videoTitle = videoTitle;
-                playerViewController.videoLength = videoLength;
-                playerViewController.videoArtwork = videoArtwork;
-                playerViewController.videoViewCount = videoViewCount;
-                playerViewController.videoLikes = videoLikes;
-                playerViewController.videoDislikes = videoDislikes;
-                playerViewController.audioURL = nil;
-                playerViewController.videoStream = videoStream;
-                playerViewController.sponsorBlockValues = sponsorBlockValues;
-
-                UINavigationController *playerViewControllerView = [[UINavigationController alloc] initWithRootViewController:playerViewController];
-                playerViewControllerView.modalPresentationStyle = UIModalPresentationFullScreen;
-
-                [topViewController presentViewController:playerViewControllerView animated:YES completion:nil];
-            }]];
-        }
-        if (audioURL != nil) {
-            [alertQualitySelector addAction:[UIAlertAction actionWithTitle:@"Audio Only" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-                PlayerViewController *playerViewController = [[PlayerViewController alloc] init];
-                playerViewController.videoID = videoID;
-                playerViewController.videoTitle = videoTitle;
-                playerViewController.videoLength = videoLength;
-                playerViewController.videoArtwork = videoArtwork;
-                playerViewController.videoViewCount = videoViewCount;
-                playerViewController.videoLikes = videoLikes;
-                playerViewController.videoDislikes = videoDislikes;
-                playerViewController.audioURL = audioURL;
-                playerViewController.videoStream = nil;
-                playerViewController.sponsorBlockValues = sponsorBlockValues;
-
-                UINavigationController *playerViewControllerView = [[UINavigationController alloc] initWithRootViewController:playerViewController];
-                playerViewControllerView.modalPresentationStyle = UIModalPresentationFullScreen;
-
-                [topViewController presentViewController:playerViewControllerView animated:YES completion:nil];
-            }]];
-        }
-
-        [alertQualitySelector addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
-        }]];
-
-        [topViewController presentViewController:alertQualitySelector animated:YES completion:nil];
+        alertQualitySelector = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet];
     } else {
-        UIAlertController *alertQualitySelector = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleAlert];
-    
-        if (videoStream != nil) {
-            [alertQualitySelector addAction:[UIAlertAction actionWithTitle:@"Stream" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-                PlayerViewController *playerViewController = [[PlayerViewController alloc] init];
-                playerViewController.videoID = videoID;
-                playerViewController.videoTitle = videoTitle;
-                playerViewController.videoLength = videoLength;
-                playerViewController.videoArtwork = videoArtwork;
-                playerViewController.videoViewCount = videoViewCount;
-                playerViewController.videoLikes = videoLikes;
-                playerViewController.videoDislikes = videoDislikes;
-                playerViewController.audioURL = nil;
-                playerViewController.videoStream = videoStream;
-                playerViewController.sponsorBlockValues = sponsorBlockValues;
-
-                UINavigationController *playerViewControllerView = [[UINavigationController alloc] initWithRootViewController:playerViewController];
-                playerViewControllerView.modalPresentationStyle = UIModalPresentationFullScreen;
-
-                [topViewController presentViewController:playerViewControllerView animated:YES completion:nil];
-            }]];
-        }
-        if (audioURL != nil) {
-            [alertQualitySelector addAction:[UIAlertAction actionWithTitle:@"Audio Only" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-                PlayerViewController *playerViewController = [[PlayerViewController alloc] init];
-                playerViewController.videoID = videoID;
-                playerViewController.videoTitle = videoTitle;
-                playerViewController.videoLength = videoLength;
-                playerViewController.videoArtwork = videoArtwork;
-                playerViewController.videoViewCount = videoViewCount;
-                playerViewController.videoLikes = videoLikes;
-                playerViewController.videoDislikes = videoDislikes;
-                playerViewController.audioURL = audioURL;
-                playerViewController.videoStream = nil;
-                playerViewController.sponsorBlockValues = sponsorBlockValues;
-
-                UINavigationController *playerViewControllerView = [[UINavigationController alloc] initWithRootViewController:playerViewController];
-                playerViewControllerView.modalPresentationStyle = UIModalPresentationFullScreen;
-
-                [topViewController presentViewController:playerViewControllerView animated:YES completion:nil];
-            }]];
-        }
-
-        [alertQualitySelector addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
-        }]];
-
-        [topViewController presentViewController:alertQualitySelector animated:YES completion:nil];
+        alertQualitySelector = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleAlert];
     }
+    
+    if (videoStream != nil) {
+        [alertQualitySelector addAction:[UIAlertAction actionWithTitle:@"Stream" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            PlayerViewController *playerViewController = [[PlayerViewController alloc] init];
+            playerViewController.videoID = videoID;
+            playerViewController.videoTitle = videoTitle;
+            playerViewController.videoLength = videoLength;
+            playerViewController.videoArtwork = videoArtwork;
+            playerViewController.videoViewCount = videoViewCount;
+            playerViewController.videoLikes = videoLikes;
+            playerViewController.videoDislikes = videoDislikes;
+            playerViewController.audioURL = nil;
+            playerViewController.videoStream = videoStream;
+            playerViewController.sponsorBlockValues = sponsorBlockValues;
+
+            UINavigationController *playerViewControllerView = [[UINavigationController alloc] initWithRootViewController:playerViewController];
+            playerViewControllerView.modalPresentationStyle = UIModalPresentationFullScreen;
+
+            [topViewController presentViewController:playerViewControllerView animated:YES completion:nil];
+        }]];
+    }
+
+    if (audioURL != nil) {
+        [alertQualitySelector addAction:[UIAlertAction actionWithTitle:@"Audio Only" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            PlayerViewController *playerViewController = [[PlayerViewController alloc] init];
+            playerViewController.videoID = videoID;
+            playerViewController.videoTitle = videoTitle;
+            playerViewController.videoLength = videoLength;
+            playerViewController.videoArtwork = videoArtwork;
+            playerViewController.videoViewCount = videoViewCount;
+            playerViewController.videoLikes = videoLikes;
+            playerViewController.videoDislikes = videoDislikes;
+            playerViewController.audioURL = audioURL;
+            playerViewController.videoStream = nil;
+            playerViewController.sponsorBlockValues = sponsorBlockValues;
+
+            UINavigationController *playerViewControllerView = [[UINavigationController alloc] initWithRootViewController:playerViewController];
+            playerViewControllerView.modalPresentationStyle = UIModalPresentationFullScreen;
+
+            [topViewController presentViewController:playerViewControllerView animated:YES completion:nil];
+        }]];
+    }
+
+    [alertQualitySelector addAction:[UIAlertAction actionWithTitle:@"Add To Playlist" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        AddToPlaylistsViewController *addToPlaylistsViewController = [[AddToPlaylistsViewController alloc] init];
+        addToPlaylistsViewController.videoID = videoID;
+
+        [topViewController presentViewController:addToPlaylistsViewController animated:YES completion:nil];
+    }]];
+
+    [alertQualitySelector addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+    }]];
+
+    [topViewController presentViewController:alertQualitySelector animated:YES completion:nil];
 }
 
 @end
