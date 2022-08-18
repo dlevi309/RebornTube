c21c8acb45806055d2f547bd5d1f83011c11406c
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-08-18 18:13:43 -0400

    Bump alpha version

--- Controllers/Settings/SettingsViewController.m
+++ Controllers/Settings/SettingsViewController.m
@@ -174,7 +174,7 @@
 
 - (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section {
     if (section == 2) {
-        return @"Version: 1.0.0 (Alpha 29)";
+        return @"Version: 1.0.0 (Alpha 30)";
     }
     return nil;
 }
