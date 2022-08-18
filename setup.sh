abdf261d38d2d7ef196bd8eee0b3779011515245
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-08-18 16:55:09 -0400

    Change from wget to curl

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 set -e
-wget https://github.com/tanersener/ffmpeg-kit/releases/download/v4.5.1.LTS/ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
+curl -LO https://github.com/tanersener/ffmpeg-kit/releases/download/v4.5.1.LTS/ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
 unzip ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip -d Resources/Frameworks
 rm -rf ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
 echo Done
