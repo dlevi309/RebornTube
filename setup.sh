c096a54714d94e47efd41836a2d741b3c7ee0a2c
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-08-28 15:31:33 -0400

    Add MobileVLCKit to setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 set -e
 curl -LO https://github.com/tanersener/ffmpeg-kit/releases/download/v4.5.1.LTS/ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
+curl -LO https://artifacts.videolan.org/VLCKit/dev-artifacts-MobileVLCKit-main/MobileVLCKit-4.0-20220629-0555.tar.xz
 unzip ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip -d Resources/Frameworks
-rm -rf ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
+tar -xf MobileVLCKit-4.0-20220629-0555.tar.xz
+mv MobileVLCKit-binary/MobileVLCKit.xcframework/ios-arm64_armv7/MobileVLCKit.framework Resources/Frameworks
+rm -rf ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip MobileVLCKit-4.0-20220629-0555.tar.xz
 echo Done
