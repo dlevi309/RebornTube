a9ceafa799f88e04517267878203461760c5b2bd
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-08-28 15:33:23 -0400

    Forgot to remove MobileVLCKit extracted folder

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ curl -LO https://artifacts.videolan.org/VLCKit/dev-artifacts-MobileVLCKit-main/M
 unzip ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip -d Resources/Frameworks
 tar -xf MobileVLCKit-4.0-20220629-0555.tar.xz
 mv MobileVLCKit-binary/MobileVLCKit.xcframework/ios-arm64_armv7/MobileVLCKit.framework Resources/Frameworks
-rm -rf ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip MobileVLCKit-4.0-20220629-0555.tar.xz
+rm -rf ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip MobileVLCKit-4.0-20220629-0555.tar.xz MobileVLCKit-binary
 echo Done
