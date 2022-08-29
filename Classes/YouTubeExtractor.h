d1ff8e043114058f9d293f37fd176dcf93ed1c77
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-08-28 21:38:40 -0400

    Add country localization to innertube requests

--- Classes/YouTubeExtractor.h
+++ Classes/YouTubeExtractor.h
@@ -2,7 +2,6 @@
 
 @interface YouTubeExtractor : NSObject
 + (NSMutableDictionary *)youtubeAndroidPlayerRequest :(NSString *)videoID;
-+ (NSMutableDictionary *)youtubeTVOSEmbedPlayerRequest :(NSString *)videoID;
 + (NSMutableDictionary *)youtubeAndroidBrowseRequest :(NSString *)browseId :(NSString *)browseParams;
 + (NSMutableDictionary *)youtubeWebSearchRequest :(NSString *)searchQuery;
 + (NSMutableDictionary *)returnYouTubeDislikeRequest :(NSString *)videoID;
--- Classes/YouTubeExtractor.m
+++ Classes/YouTubeExtractor.m
@@ -3,23 +3,14 @@
 @implementation YouTubeExtractor
 
 + (NSMutableDictionary *)youtubeAndroidPlayerRequest :(NSString *)videoID {
-    NSMutableURLRequest *innertubeRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false"]];
-    [innertubeRequest setHTTPMethod:@"POST"];
-    [innertubeRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-    [innertubeRequest setValue:@"CONSENT=YES+" forHTTPHeaderField:@"Cookie"];
-    NSString *jsonBody = [NSString stringWithFormat:@"{\"context\":{\"client\":{\"hl\":\"en\",\"gl\":\"US\",\"clientName\":\"ANDROID\",\"clientVersion\":\"16.20\",\"playbackContext\":{\"contentPlaybackContext\":{\"signatureTimestamp\":\"sts\",\"html5Preference\":\"HTML5_PREF_WANTS\"}}}},\"contentCheckOk\":true,\"racyCheckOk\":true,\"videoId\":\"%@\"}", videoID];
-    [innertubeRequest setHTTPBody:[jsonBody dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES]];
+    NSLocale *locale = [NSLocale currentLocale];
+	NSString *countryCode = [locale objectForKey:NSLocaleCountryCode];
     
-    NSData *data = [NSURLConnection sendSynchronousRequest:innertubeRequest returningResponse:nil error:nil];
-    return [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-}
-
-+ (NSMutableDictionary *)youtubeTVOSEmbedPlayerRequest :(NSString *)videoID {
     NSMutableURLRequest *innertubeRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false"]];
     [innertubeRequest setHTTPMethod:@"POST"];
     [innertubeRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     [innertubeRequest setValue:@"CONSENT=YES+" forHTTPHeaderField:@"Cookie"];
-    NSString *jsonBody = [NSString stringWithFormat:@"{\"context\":{\"client\":{\"hl\":\"en\",\"gl\":\"US\",\"clientName\":\"TVHTML5_SIMPLY_EMBEDDED_PLAYER\",\"clientVersion\":\"2.0\",\"playbackContext\":{\"contentPlaybackContext\":{\"signatureTimestamp\":\"sts\",\"html5Preference\":\"HTML5_PREF_WANTS\"}}}},\"contentCheckOk\":true,\"racyCheckOk\":true,\"videoId\":\"%@\"}", videoID];
+    NSString *jsonBody = [NSString stringWithFormat:@"{\"context\":{\"client\":{\"hl\":\"en\",\"gl\":\"%@\",\"clientName\":\"ANDROID\",\"clientVersion\":\"16.20\",\"playbackContext\":{\"contentPlaybackContext\":{\"signatureTimestamp\":\"sts\",\"html5Preference\":\"HTML5_PREF_WANTS\"}}}},\"contentCheckOk\":true,\"racyCheckOk\":true,\"videoId\":\"%@\"}", countryCode, videoID];
     [innertubeRequest setHTTPBody:[jsonBody dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES]];
     
     NSData *data = [NSURLConnection sendSynchronousRequest:innertubeRequest returningResponse:nil error:nil];
@@ -27,11 +18,14 @@
 }
 
 + (NSMutableDictionary *)youtubeAndroidBrowseRequest :(NSString *)browseId :(NSString *)browseParams {
+    NSLocale *locale = [NSLocale currentLocale];
+	NSString *countryCode = [locale objectForKey:NSLocaleCountryCode];
+    
     NSMutableURLRequest *innertubeRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://www.youtube.com/youtubei/v1/browse?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false"]];
     [innertubeRequest setHTTPMethod:@"POST"];
     [innertubeRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     [innertubeRequest setValue:@"CONSENT=YES+" forHTTPHeaderField:@"Cookie"];
-    NSString *jsonBody = [NSString stringWithFormat:@"{\"context\":{\"client\":{\"hl\":\"en\",\"gl\":\"US\",\"clientName\":\"ANDROID\",\"clientVersion\":\"16.20\",\"playbackContext\":{\"contentPlaybackContext\":{\"signatureTimestamp\":\"sts\",\"html5Preference\":\"HTML5_PREF_WANTS\"}}}},\"contentCheckOk\":true,\"racyCheckOk\":true,\"browseId\":\"%@\",\"params\":\"%@\"}", browseId, browseParams];
+    NSString *jsonBody = [NSString stringWithFormat:@"{\"context\":{\"client\":{\"hl\":\"en\",\"gl\":\"%@\",\"clientName\":\"ANDROID\",\"clientVersion\":\"16.20\",\"playbackContext\":{\"contentPlaybackContext\":{\"signatureTimestamp\":\"sts\",\"html5Preference\":\"HTML5_PREF_WANTS\"}}}},\"contentCheckOk\":true,\"racyCheckOk\":true,\"browseId\":\"%@\",\"params\":\"%@\"}", countryCode, browseId, browseParams];
     [innertubeRequest setHTTPBody:[jsonBody dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES]];
     
     NSData *data = [NSURLConnection sendSynchronousRequest:innertubeRequest returningResponse:nil error:nil];
@@ -39,11 +33,14 @@
 }
 
 + (NSMutableDictionary *)youtubeWebSearchRequest :(NSString *)searchQuery {
+    NSLocale *locale = [NSLocale currentLocale];
+	NSString *countryCode = [locale objectForKey:NSLocaleCountryCode];
+    
     NSMutableURLRequest *innertubeRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://www.youtube.com/youtubei/v1/search?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false"]];
     [innertubeRequest setHTTPMethod:@"POST"];
     [innertubeRequest setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     [innertubeRequest setValue:@"CONSENT=YES+" forHTTPHeaderField:@"Cookie"];
-    NSString *jsonBody = [NSString stringWithFormat:@"{\"context\":{\"client\":{\"hl\":\"en\",\"gl\":\"US\",\"clientName\":\"WEB\",\"clientVersion\":\"2.20210401.08.00\",\"playbackContext\":{\"contentPlaybackContext\":{\"signatureTimestamp\":\"sts\",\"html5Preference\":\"HTML5_PREF_WANTS\"}}}},\"contentCheckOk\":true,\"racyCheckOk\":true,\"query\":\"%@\"}", searchQuery];
+    NSString *jsonBody = [NSString stringWithFormat:@"{\"context\":{\"client\":{\"hl\":\"en\",\"gl\":\"%@\",\"clientName\":\"WEB\",\"clientVersion\":\"2.20210401.08.00\",\"playbackContext\":{\"contentPlaybackContext\":{\"signatureTimestamp\":\"sts\",\"html5Preference\":\"HTML5_PREF_WANTS\"}}}},\"contentCheckOk\":true,\"racyCheckOk\":true,\"query\":\"%@\"}", countryCode, searchQuery];
     [innertubeRequest setHTTPBody:[jsonBody dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES]];
     
     NSData *data = [NSURLConnection sendSynchronousRequest:innertubeRequest returningResponse:nil error:nil];
@@ -51,7 +48,7 @@
 }
 
 + (NSMutableDictionary *)returnYouTubeDislikeRequest :(NSString *)videoID {
-    NSMutableURLRequest *innertubeRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://returnyoutubedislikeapi.com/votes?videoId=%@", videoID]]];
+    NSURLRequest *innertubeRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://returnyoutubedislikeapi.com/votes?videoId=%@", videoID]]];
     
     NSData *data = [NSURLConnection sendSynchronousRequest:innertubeRequest returningResponse:nil error:nil];
     return [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
@@ -59,7 +56,7 @@
 
 + (NSMutableDictionary *)sponsorBlockRequest :(NSString *)videoID {
     NSString *options = @"[%22sponsor%22,%22selfpromo%22,%22interaction%22,%22intro%22,%22outro%22,%22preview%22,%22music_offtopic%22]";
-    NSMutableURLRequest *innertubeRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://sponsor.ajay.app/api/skipSegments?videoID=%@&categories=%@", videoID, options]]];
+    NSURLRequest *innertubeRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://sponsor.ajay.app/api/skipSegments?videoID=%@&categories=%@", videoID, options]]];
     
     NSData *data = [NSURLConnection sendSynchronousRequest:innertubeRequest returningResponse:nil error:nil];
     NSMutableDictionary *jsonResponse = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
