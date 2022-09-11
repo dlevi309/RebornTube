71bf6eb187f84ba7d75c611e42de91166af09fac
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-09-11 19:10:43 -0400

    Remove items at the beginning of setup script incase error or update

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 set -e
+rm -rf Resources/Frameworks ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip MobileVLCKit-4.0-20220906-0249.tar.xz MobileVLCKit-binary
 curl -LO https://github.com/tanersener/ffmpeg-kit/releases/download/v4.5.1.LTS/ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
 curl -LO https://artifacts.videolan.org/VLCKit/dev-artifacts-MobileVLCKit-main/MobileVLCKit-4.0-20220906-0249.tar.xz
 unzip ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip -d Resources/Frameworks
