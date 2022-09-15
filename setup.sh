a9550ebe3818d83ba778b1c8886d86d30d367a9b
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-09-15 17:17:26 -0400

    Update ffmpegkit download link

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 set -e
 rm -rf Resources/Frameworks Temp ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip Sentry.framework.zip
-curl -LO https://github.com/arthenica/ffmpeg-kit/releases/download/v4.5.1.LTS/ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
+curl -LO https://github.com/tanersener/ffmpeg-kit/releases/download/v4.5.1.LTS/ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
 curl -LO https://github.com/getsentry/sentry-cocoa/releases/download/7.25.1/Sentry.framework.zip
 unzip ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip -d Resources/Frameworks
 unzip Sentry.framework.zip -d Temp
