ec82b7254904e98ebd754ad6e29b0ee80ff1ac81
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-09-14 21:09:47 -0400

    Change ffmpegkit download link to new github

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 set -e
 rm -rf Resources/Frameworks ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
-curl -LO https://github.com/tanersener/ffmpeg-kit/releases/download/v4.5.1.LTS/ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
+curl -LO https://github.com/arthenica/ffmpeg-kit/releases/download/v4.5.1.LTS/ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
 unzip ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip -d Resources/Frameworks
 rm -rf ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
 echo Done
