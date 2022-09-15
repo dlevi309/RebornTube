27fe6b77916ef5f5d6b477856acd8c0e6b500f28
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-09-14 23:22:24 -0400

    Add Sentry framework to project

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 set -e
-rm -rf Resources/Frameworks ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
+rm -rf Resources/Frameworks Temp ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip Sentry.framework.zip
 curl -LO https://github.com/arthenica/ffmpeg-kit/releases/download/v4.5.1.LTS/ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
+curl -LO https://github.com/getsentry/sentry-cocoa/releases/download/7.25.1/Sentry.framework.zip
 unzip ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip -d Resources/Frameworks
-rm -rf ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
+unzip Sentry.framework.zip -d Temp
+mv Temp/Carthage/Build/iOS/Sentry.framework Resources/Frameworks
+rm -rf Temp ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip Sentry.framework.zip
 echo Done
