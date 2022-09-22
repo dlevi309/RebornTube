bc1d51a54b6c78247e017618fa666589cb07d739
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-09-21 22:46:21 -0400

    Cleanup AppDelegate

--- Classes/AppDelegate.m
+++ Classes/AppDelegate.m
@@ -1,9 +1,6 @@
 #import "AppDelegate.h"
 #import "AppColours.h"
-#import "AppHistory.h"
-#import "YouTubeLoader.h"
 #import "../Controllers/RootViewController.h"
-#import "../Controllers/Player/PlayerViewController.h"
 
 @implementation AppDelegate
 
@@ -38,20 +35,5 @@
     [self.window makeKeyAndVisible];
     return YES;
 }
-
-/* - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options {
-    NSString *returnURL = [NSString stringWithFormat:@"%@", url];
-    NSString *regexString = @"((?<=(v|V)/)|(?<=be/)|(?<=(\\?|\\&)v=)|(?<=embed/))([\\w-]++)";
-    NSRegularExpression *regExp = [NSRegularExpression regularExpressionWithPattern:regexString options:NSRegularExpressionCaseInsensitive error:nil];
-	NSArray *array = [regExp matchesInString:returnURL options:0 range:NSMakeRange(0, returnURL.length)];
-    if (array.count > 0) {
-        NSTextCheckingResult *result = array.firstObject;
-		NSString *videoID = [returnURL substringWithRange:result.range];
-
-        [AppHistory init:videoID];
-        [YouTubeLoader init:videoID];
-    }
-    return YES;
-} */
 
 @end
