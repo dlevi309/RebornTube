8807b3d643170095101550d4c36785de4e2a6c99
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-09-25 15:49:55 -0400

    Fix missing stuff from playlists view controller

--- Controllers/Playlists/PlaylistsViewController.m
+++ Controllers/Playlists/PlaylistsViewController.m
@@ -11,6 +11,11 @@
 // Classes
 
 #import "../../Classes/AppColours.h"
+#import "../../Classes/AppFonts.h"
+
+// Controllers
+
+#import "CreatePlaylistsViewController.h"
 
 // Views
 
@@ -79,8 +84,27 @@
 }
 
 - (void)mainViewSetup {
+	UIView *createPlaylistsView = [[UIView alloc] init];
+	createPlaylistsView.frame = CGRectMake(0, boundsWindow.safeAreaInsets.top + self.navigationController.navigationBar.frame.size.height, self.view.bounds.size.width, 40);
+	createPlaylistsView.backgroundColor = [AppColours viewBackgroundColour];
+	createPlaylistsView.userInteractionEnabled = YES;
+    UITapGestureRecognizer *createPlaylistsViewTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(createPlaylistsTap:)];
+    createPlaylistsViewTap.numberOfTapsRequired = 1;
+    [createPlaylistsView addGestureRecognizer:createPlaylistsViewTap];
+	
+	UILabel *createPlaylistsLabel = [[UILabel alloc] init];
+    createPlaylistsLabel.frame = CGRectMake(10, 0, createPlaylistsView.frame.size.width - 10, createPlaylistsView.frame.size.height);
+    createPlaylistsLabel.text = @"Create Playlist";
+    createPlaylistsLabel.textColor = [AppColours textColour];
+    createPlaylistsLabel.numberOfLines = 1;
+	[createPlaylistsLabel setFont:[AppFonts mainFont:createPlaylistsLabel.font.pointSize]];
+    createPlaylistsLabel.adjustsFontSizeToFitWidth = YES;
+    [createPlaylistsView addSubview:createPlaylistsLabel];
+
+	[self.view addSubview:createPlaylistsView];
+
 	[scrollView removeFromSuperview];
-	scrollView.frame = CGRectMake(boundsWindow.safeAreaInsets.left, boundsWindow.safeAreaInsets.top + self.navigationController.navigationBar.frame.size.height, self.view.bounds.size.width - boundsWindow.safeAreaInsets.left - boundsWindow.safeAreaInsets.right, self.view.bounds.size.height - boundsWindow.safeAreaInsets.top - self.navigationController.navigationBar.frame.size.height - boundsWindow.safeAreaInsets.bottom);
+	scrollView.frame = CGRectMake(boundsWindow.safeAreaInsets.left, boundsWindow.safeAreaInsets.top + self.navigationController.navigationBar.frame.size.height + createPlaylistsLabel.frame.size.height + 2, self.view.bounds.size.width - boundsWindow.safeAreaInsets.left - boundsWindow.safeAreaInsets.right, self.view.bounds.size.height - boundsWindow.safeAreaInsets.top - self.navigationController.navigationBar.frame.size.height - boundsWindow.safeAreaInsets.bottom - createPlaylistsLabel.frame.size.height - 2);
 	scrollView.refreshControl = [UIRefreshControl new];
     [scrollView.refreshControl addTarget:self action:@selector(refresh:) forControlEvents:UIControlEventValueChanged];
 
@@ -142,6 +166,14 @@
 	SettingsNavigationController *settingsNavigationController = [[SettingsNavigationController alloc] initWithRootViewController:settingsViewController];
     
 	[self presentViewController:settingsNavigationController animated:YES completion:nil];
+}
+
+// Create Playlists
+
+- (void)createPlaylistsTap:(UITapGestureRecognizer *)recognizer {
+    CreatePlaylistsViewController *createPlaylistsViewController = [[CreatePlaylistsViewController alloc] init];
+
+    [self.navigationController pushViewController:createPlaylistsViewController animated:YES];
 }
 
 // Scroll View
