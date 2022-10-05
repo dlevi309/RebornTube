d28c05fa41d94d2ef02620871a2dff9ddb4d6b4c
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-10-05 18:50:07 -0400

    Implement new setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 set -e
-rm -rf Resources/Frameworks ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
-curl -LO https://github.com/tanersener/ffmpeg-kit/releases/download/v4.5.1.LTS/ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
-unzip ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip -d Resources/Frameworks
-rm -rf ffmpeg-kit-full-4.5.1.LTS-ios-framework.zip
+rm -rf Resources/Frameworks ffmpeg-kit-full-5.1.LTS-ios-framework.zip MobileVLCKit-4.0-20220924-1124.tar.xz MobileVLCKit-binary
+curl -LO https://github.com/arthenica/ffmpeg-kit/releases/download/v5.1.LTS/ffmpeg-kit-full-5.1.LTS-ios-framework.zip
+curl -LO https://artifacts.videolan.org/VLCKit/dev-artifacts-MobileVLCKit-main/MobileVLCKit-4.0-20220924-1124.tar.xz
+unzip ffmpeg-kit-full-5.1.LTS-ios-framework.zip -d Resources/Frameworks
+tar -xf MobileVLCKit-4.0-20220924-1124.tar.xz
+mv MobileVLCKit-binary/MobileVLCKit.xcframework/ios-arm64_armv7/MobileVLCKit.framework Resources/Frameworks
+rm -rf ffmpeg-kit-full-5.1.LTS-ios-framework.zip MobileVLCKit-4.0-20220924-1124.tar.xz MobileVLCKit-binary
 echo Done
