4a83eddb1ebf033bc5967f9c63a9bfcf9f106c37
Lillie <91358136+LillieH001@users.noreply.github.com>
2022-10-05 23:21:52 -0400

    Improve navbar and tabbar handling in appdelegate

--- Classes/AppDelegate.m
+++ Classes/AppDelegate.m
@@ -5,31 +5,20 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	if (@available(iOS 15.0, *)){
-        UINavigationBarAppearance *navBarAppearance = [[UINavigationBarAppearance alloc] init];
-        [navBarAppearance configureWithOpaqueBackground];
-        navBarAppearance.titleTextAttributes = @{NSForegroundColorAttributeName : [AppColours textColour]};
-        navBarAppearance.backgroundColor = [AppColours mainBackgroundColour];
-        [UINavigationBar appearance].standardAppearance = navBarAppearance;
+	UINavigationBarAppearance *navBarAppearance = [[UINavigationBarAppearance alloc] init];
+    [navBarAppearance configureWithOpaqueBackground];
+    navBarAppearance.titleTextAttributes = @{NSForegroundColorAttributeName : [AppColours textColour]};
+    navBarAppearance.backgroundColor = [AppColours mainBackgroundColour];
+    [UINavigationBar appearance].standardAppearance = navBarAppearance;
+
+    UITabBarAppearance *tabBarAppearance = [[UITabBarAppearance alloc] init];
+    [tabBarAppearance configureWithOpaqueBackground];
+    tabBarAppearance.backgroundColor = [AppColours mainBackgroundColour];
+    [UITabBar appearance].standardAppearance = tabBarAppearance;
+    if (@available(iOS 15.0, *)){
         [UINavigationBar appearance].scrollEdgeAppearance = navBarAppearance;
-
-		UITabBarAppearance *tabBarAppearance = [[UITabBarAppearance alloc] init];
-		[tabBarAppearance configureWithOpaqueBackground];
-        tabBarAppearance.backgroundColor = [AppColours mainBackgroundColour];
-		[UITabBar appearance].standardAppearance = tabBarAppearance;
         [UITabBar appearance].scrollEdgeAppearance = tabBarAppearance;
-    } else {
-		UINavigationBarAppearance *navBarAppearance = [[UINavigationBarAppearance alloc] init];
-        [navBarAppearance configureWithOpaqueBackground];
-        navBarAppearance.titleTextAttributes = @{NSForegroundColorAttributeName : [AppColours textColour]};
-        navBarAppearance.backgroundColor = [AppColours mainBackgroundColour];
-        [UINavigationBar appearance].standardAppearance = navBarAppearance;
-
-		UITabBarAppearance *tabBarAppearance = [[UITabBarAppearance alloc] init];
-		[tabBarAppearance configureWithOpaqueBackground];
-        tabBarAppearance.backgroundColor = [AppColours mainBackgroundColour];
-		[UITabBar appearance].standardAppearance = tabBarAppearance;
-	}
+    }
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.rootViewController = [[RootViewController alloc] init];
     [self.window makeKeyAndVisible];
