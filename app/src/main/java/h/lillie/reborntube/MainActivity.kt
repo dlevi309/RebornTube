b4c7c37893b382ed068440306cc50a5053b620ff
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-01-15 21:15:22 -0500

    Add files

---
+++ app/src/main/java/h/lillie/reborntube/MainActivity.kt
@@ -0,0 +1,11 @@
+package h.lillie.reborntube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
