aec7907c5cfd43e56b70cf6090294831e3ad7f9b
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-01-28 18:13:12 -0500

    Change from retrofit to okhttp, change videoview ui handling

--- app/src/main/java/h/lillie/reborntube/Main.kt
+++ app/src/main/java/h/lillie/reborntube/Main.kt
@@ -2,63 +2,128 @@ package h.lillie.reborntube
 
 import android.net.Uri
 import android.os.Bundle
-import android.widget.VideoView
+import android.util.DisplayMetrics
+import android.util.Log
 import android.widget.MediaController
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
-import h.lillie.reborntube.playerapi.PlayerApiRequest
-import retrofit2.Retrofit
-import retrofit2.awaitResponse
-import retrofit2.converter.scalars.ScalarsConverterFactory
-import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.*
+import okhttp3.RequestBody.Companion.toRequestBody
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.IOException
+import org.json.JSONObject
 
 class Main : AppCompatActivity() {
+
+    private var deviceHeight = 0;
+    private var deviceWidth = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+        getDeviceInfo()
+        setupUI()
         getInfo()
     }
 
-    private fun getInfo(){
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://www.youtube.com")
-            .addConverterFactory(ScalarsConverterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(PlayerApiRequest::class.java)
+    private fun getDeviceInfo() {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        deviceHeight = displayMetrics.heightPixels
+        deviceWidth = displayMetrics.widthPixels
+    }
 
-        GlobalScope.launch(Dispatchers.IO) {
-            var body = """{
-                "context": {
-                    "client": {
-                        "hl": "en",
-                        "gl": "US",
-                        "clientName": "IOS",
-                        "clientVersion": "16.20",
-                        "playbackContext": {
-                            "contentPlaybackContext": {
-                                "signatureTimestamp": "sts",
-                                "html5Preference": "HTML5_PREF_WANTS"
-                            }
+    private fun setupUI() {
+        var videoView: VideoView = findViewById(R.id.videoView)
+        videoView.getLayoutParams().height = deviceWidth * 9 / 16
+        videoView.getLayoutParams().width = deviceWidth
+        videoView.requestLayout()
+    }
+
+    private fun getInfo() {
+        val body = """{
+            "context": {
+                "client": {
+                    "hl": "en",
+                    "gl": "US",
+                    "clientName": "ANDROID",
+                    "clientVersion": "16.20",
+                    "playbackContext": {
+                        "contentPlaybackContext": {
+                            "signatureTimestamp": "sts",
+                            "html5Preference": "HTML5_PREF_WANTS"
                         }
                     }
-                },
-                "contentCheckOk": true,
-                "racyCheckOk": true,
-                "videoId": "ytWz0qVvBZ0"
-            }""".trimIndent()
-            val response = retrofit.getPlayerApiValues(body).awaitResponse()
-            if (response.isSuccessful) {
-                val data = response.body()!!
-                withContext(Dispatchers.Main) {
-                    createPlayer(data.streamingData.hlsManifestUrl)
+                }
+            },
+            "contentCheckOk": true,
+            "racyCheckOk": true,
+            "videoId": "ytWz0qVvBZ0"
+        }""".trimIndent()
+
+        val requestBody = body.toRequestBody()
+
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .build()
+
+        val request = Request.Builder()
+            .method("POST", requestBody)
+            .url("https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.d("Failure", e.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseBody = response.body.string()
+                response.body.close()
+                response.close()
+
+                var url = ""
+
+                var q720p = ""
+                var q480p = ""
+                var q360p = ""
+                var q240p = ""
+                val jsonObject = JSONObject(responseBody)
+                val jsonArray = jsonObject.getJSONObject("streamingData").getJSONArray("formats")
+                for (i in 0 until jsonArray.length()) {
+                    val mimeType = jsonArray.getJSONObject(i).getString("mimeType")
+                    val height = jsonArray.getJSONObject(i).getString("height")
+                    val quality = jsonArray.getJSONObject(i).getString("quality")
+                    if (mimeType.contains("video/mp4") && height.contains("720") || mimeType.contains("video/mp4") && quality.contains("hd720")) {
+                        q720p = jsonArray.getJSONObject(i).getString("url")
+                    } else if (mimeType.contains("video/mp4") && height.contains("480") || mimeType.contains("video/mp4") && quality.contains("480p")) {
+                        q480p = jsonArray.getJSONObject(i).getString("url")
+                    } else if (mimeType.contains("video/mp4") && height.contains("360") || mimeType.contains("video/mp4") && quality.contains("360p")) {
+                        q360p = jsonArray.getJSONObject(i).getString("url")
+                    } else if (mimeType.contains("video/mp4") && height.contains("240") || mimeType.contains("video/mp4") && quality.contains("240p")) {
+                        q240p = jsonArray.getJSONObject(i).getString("url")
+                    }
+                }
+
+                if (q720p != null) {
+                    url = q720p
+                } else if (q480p != null) {
+                    url = q480p
+                } else if (q360p != null) {
+                    url = q360p
+                } else if (q240p != null) {
+                    url = q240p
+                }
+
+                GlobalScope.launch(Dispatchers.IO) {
+                    withContext(Dispatchers.Main) {
+                        createPlayer(q720p)
+                    }
                 }
             }
-        }
+        })
     }
 
     private fun createPlayer(videoUrl: String) {
--- app/src/main/java/h/lillie/reborntube/playerapi/PlayerApiValues.kt
+++
@@ -1,9 +0,0 @@
-package h.lillie.reborntube.playerapi
-
-data class PlayerApiValues (
-    val streamingData: HlsManifestUrl
-)
-
-data class HlsManifestUrl (
-    val hlsManifestUrl: String
-)
