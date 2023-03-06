16107ea63a4596c5866f5b9da0599586118fdab9
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-03-05 20:22:14 -0500

    Add basic overlay controls to vlc player

--- app/src/main/java/h/lillie/reborntube/VLCPlayer.kt
+++ app/src/main/java/h/lillie/reborntube/VLCPlayer.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.widget.RelativeLayout
+import android.widget.Button
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.content.res.Configuration
@@ -86,6 +87,32 @@ class VLCPlayer : AppCompatActivity() {
         // Video Player
         videoLayout = findViewById(R.id.videoLayout)
         videoLayout.layoutParams = RelativeLayout.LayoutParams(deviceWidth, deviceWidth * 9 / 16)
+
+        // Left Overlay
+        var rewindButton: Button = findViewById(R.id.rewindButton)
+        rewindButton.layoutParams = RelativeLayout.LayoutParams(deviceWidth / 3, deviceWidth * 9 / 16)
+        rewindButton.setOnClickListener {
+        }
+
+        // Middle Overlay
+        var playButton: Button = findViewById(R.id.playButton)
+        playButton.layoutParams = RelativeLayout.LayoutParams(deviceWidth / 3, deviceWidth * 9 / 16)
+        playButton.x = deviceWidth / 3.toFloat()
+        playButton.setOnClickListener {
+            val playbackState = mediaPlayer.isPlaying
+            if (playbackState) {
+                mediaPlayer.pause()
+            } else if (!playbackState) {
+                mediaPlayer.play()
+            }
+        }
+
+        // Right Overlay
+        var forwardButton: Button = findViewById(R.id.forwardButton)
+        forwardButton.layoutParams = RelativeLayout.LayoutParams(deviceWidth / 3, deviceWidth * 9 / 16)
+        forwardButton.x = (deviceWidth / 3) * 2.toFloat()
+        forwardButton.setOnClickListener {
+        }
     }
 
     private fun createPlayer() {
