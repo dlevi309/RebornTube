23ae4975ab4677849b33875557961e9dfa3e81b1
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-03-22 13:49:39 -0400

    Add yt browse and search innertube api requests

--- app/src/main/java/h/lillie/reborntube/Extractor.kt
+++ app/src/main/java/h/lillie/reborntube/Extractor.kt
@@ -38,6 +38,73 @@ class Extractor {
         return client.newCall(request).execute().body.string()
     }
 
+    fun browseRequest(clientName: String, clientVersion: String, browseID: String, params: String) : String {
+        val body = """{
+            "context": {
+                "client": {
+                    "hl": "en",
+                    "gl": "US",
+                    "clientName": "$clientName",
+                    "clientVersion": "$clientVersion",
+                    "playbackContext": {
+                        "contentPlaybackContext": {
+                            "signatureTimestamp": "sts",
+                            "html5Preference": "HTML5_PREF_WANTS"
+                        }
+                    }
+                }
+            },
+            "contentCheckOk": true,
+            "racyCheckOk": true,
+            "browseId": "$browseID",
+            "params": "$params"
+        }"""
+
+        val requestBody = body.trimIndent().toRequestBody()
+
+        val client: OkHttpClient = OkHttpClient.Builder().build()
+
+        val request = Request.Builder()
+            .method("POST", requestBody)
+            .url("https://www.youtube.com/youtubei/v1/browse?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false")
+            .build()
+
+        return client.newCall(request).execute().body.string()
+    }
+
+    fun searchRequest(clientName: String, clientVersion: String, query: String) : String {
+        val body = """{
+            "context": {
+                "client": {
+                    "hl": "en",
+                    "gl": "US",
+                    "clientName": "$clientName",
+                    "clientVersion": "$clientVersion",
+                    "playbackContext": {
+                        "contentPlaybackContext": {
+                            "signatureTimestamp": "sts",
+                            "html5Preference": "HTML5_PREF_WANTS"
+                        }
+                    }
+                }
+            },
+            "contentCheckOk": true,
+            "racyCheckOk": true,
+            "query": "$query"
+        }"""
+
+        val requestBody = body.trimIndent().toRequestBody()
+
+        val client: OkHttpClient = OkHttpClient.Builder().build()
+
+        val request = Request.Builder()
+            .method("POST", requestBody)
+            .url("https://www.youtube.com/youtubei/v1/search?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false")
+            .build()
+
+        return client.newCall(request).execute().body.string()
+    }
+
     fun returnYouTubeDislikesRequest(videoID: String) : String {
         val client: OkHttpClient = OkHttpClient.Builder().build()
 
