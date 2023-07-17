5938b062a10b27554d265c089892c04c8954d6b4
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-07-16 21:14:14 -0400

    Add yt url search feature

--- app/src/main/java/h/lillie/reborntube/Search.kt
+++ app/src/main/java/h/lillie/reborntube/Search.kt
@@ -44,68 +44,124 @@ class Search : AppCompatActivity() {
         searchBarButton.setOnClickListener {
             searchScrollView.removeAllViews()
             val extractor = Extractor()
-            val searchRequest = extractor.searchRequest(applicationContext, searchBar.text.toString())
-            val jsonObject = JSONObject(searchRequest)
 
-            val searchContents: JSONArray = jsonObject.getJSONObject("contents").getJSONObject("twoColumnSearchResultsRenderer").getJSONObject("primaryContents").getJSONObject("sectionListRenderer").getJSONArray("contents").getJSONObject(0).getJSONObject("itemSectionRenderer").getJSONArray("contents")
+            val youtubeRegex = Regex("^.*(?:(?:youtu\\.be\\/|v\\/|vi\\/|u\\/\\w\\/|embed\\/|shorts\\/|live\\/)|(?:(?:watch)?\\?v(?:i)?=|\\&v(?:i)?=))([^#\\&\\?]*).*")
+            if (youtubeRegex.containsMatchIn(searchBar.text)) {
+                val result = youtubeRegex.findAll(searchBar.text).map { it.groupValues[1] }.joinToString()
+                val playerRequest = extractor.playerRequest(applicationContext, result)
+                val jsonObject = JSONObject(playerRequest)
 
-            var x = 0
-            for (i in 0 until searchContents.length()) {
-                try {
-                    val searchRelativeView = RelativeLayout(applicationContext)
-                    searchRelativeView.layoutParams = RelativeLayout.LayoutParams(deviceWidth, 156)
-                    searchRelativeView.setBackgroundColor(applicationContext.getColor(R.color.darkgrey))
+                val searchRelativeView = RelativeLayout(applicationContext)
+                searchRelativeView.layoutParams = RelativeLayout.LayoutParams(deviceWidth, 156)
+                searchRelativeView.setBackgroundColor(applicationContext.getColor(R.color.darkgrey))
 
-                    val videoID: String = searchContents.getJSONObject(x).getJSONObject("videoRenderer").optString("videoId").toString()
-                    val videoTitle: String = searchContents.getJSONObject(x).getJSONObject("videoRenderer").getJSONObject("title").getJSONArray("runs").getJSONObject(0).optString("text").toString()
+                val videoID: String = jsonObject.getJSONObject("videoDetails").optString("videoId").toString()
+                val videoTitle: String = jsonObject.getJSONObject("videoDetails").optString("title").toString()
 
-                    val videoTitleTextView = TextView(applicationContext)
-                    videoTitleTextView.layoutParams = LinearLayout.LayoutParams(deviceWidth, 156)
-                    videoTitleTextView.text = videoTitle
-                    videoTitleTextView.gravity = Gravity.CENTER_VERTICAL
-                    videoTitleTextView.setTextColor(applicationContext.getColor(R.color.white))
+                val videoTitleTextView = TextView(applicationContext)
+                videoTitleTextView.layoutParams = LinearLayout.LayoutParams(deviceWidth, 156)
+                videoTitleTextView.text = videoTitle
+                videoTitleTextView.gravity = Gravity.CENTER_VERTICAL
+                videoTitleTextView.setTextColor(applicationContext.getColor(R.color.white))
 
-                    val videoButton = View(applicationContext)
-                    videoButton.layoutParams = LinearLayout.LayoutParams(deviceWidth, 156)
-                    videoButton.setOnClickListener {
-                        val playerRequest = extractor.playerRequest(applicationContext, videoID)
-                        val dislikesRequest = extractor.returnYouTubeDislikesRequest(videoID)
-                        val sponsorBlockRequest = extractor.sponsorBlockRequest(videoID)
+                val videoButton = View(applicationContext)
+                videoButton.layoutParams = LinearLayout.LayoutParams(deviceWidth, 156)
+                videoButton.setOnClickListener {
+                    val dislikesRequest = extractor.returnYouTubeDislikesRequest(videoID)
+                    val sponsorBlockRequest = extractor.sponsorBlockRequest(videoID)
 
-                        val loader = Loader()
-                        val loaderPlayerInfo = loader.playerInit(playerRequest)
-                        val loaderDislikesInfo = loader.dislikesInit(dislikesRequest)
+                    val loader = Loader()
+                    val loaderPlayerInfo = loader.playerInit(playerRequest)
+                    val loaderDislikesInfo = loader.dislikesInit(dislikesRequest)
 
-                        val gson = Gson()
-                        Application.setVideoData(gson.toJson(Data(
-                            loaderPlayerInfo[0].toString(),
-                            loaderPlayerInfo[1].toString(),
-                            sponsorBlockRequest,
-                            loaderPlayerInfo[2].toString(),
-                            loaderPlayerInfo[3].toString(),
-                            loaderPlayerInfo[4].toString(),
-                            loaderPlayerInfo[5].toString(),
-                            loaderPlayerInfo[6].toString().toBoolean(),
-                            loaderDislikesInfo[0].toString(),
-                            loaderDislikesInfo[1].toString()
-                        )))
+                    val gson = Gson()
+                    Application.setVideoData(gson.toJson(Data(
+                        loaderPlayerInfo[0].toString(),
+                        loaderPlayerInfo[1].toString(),
+                        sponsorBlockRequest,
+                        loaderPlayerInfo[2].toString(),
+                        loaderPlayerInfo[3].toString(),
+                        loaderPlayerInfo[4].toString(),
+                        loaderPlayerInfo[5].toString(),
+                        loaderPlayerInfo[6].toString().toBoolean(),
+                        loaderDislikesInfo[0].toString(),
+                        loaderDislikesInfo[1].toString()
+                    )))
 
-                        val intent = Intent(this@Search, Player::class.java)
-                        startActivity(intent)
-                    }
-                    searchRelativeView.addView(videoTitleTextView)
-                    searchRelativeView.addView(videoButton)
-
-                    val spaceView = Space(applicationContext)
-                    spaceView.minimumHeight = 4
-                    searchScrollView.addView(spaceView)
-                    searchScrollView.addView(searchRelativeView)
-                } catch (e: IOException) {
-                    Log.e("IOException", e.toString())
-                } catch (e: JSONException) {
-                    Log.e("JSONException", e.toString())
+                    val intent = Intent(this@Search, Player::class.java)
+                    startActivity(intent)
                 }
-                x += 1
+                searchRelativeView.addView(videoTitleTextView)
+                searchRelativeView.addView(videoButton)
+
+                val spaceView = Space(applicationContext)
+                spaceView.minimumHeight = 4
+                searchScrollView.addView(spaceView)
+                searchScrollView.addView(searchRelativeView)
+            } else {
+                val searchRequest = extractor.searchRequest(applicationContext, searchBar.text.toString())
+                val jsonObject = JSONObject(searchRequest)
+
+                val searchContents: JSONArray = jsonObject.getJSONObject("contents").getJSONObject("twoColumnSearchResultsRenderer").getJSONObject("primaryContents").getJSONObject("sectionListRenderer").getJSONArray("contents").getJSONObject(0).getJSONObject("itemSectionRenderer").getJSONArray("contents")
+
+                var x = 0
+                for (i in 0 until searchContents.length()) {
+                    try {
+                        val searchRelativeView = RelativeLayout(applicationContext)
+                        searchRelativeView.layoutParams = RelativeLayout.LayoutParams(deviceWidth, 156)
+                        searchRelativeView.setBackgroundColor(applicationContext.getColor(R.color.darkgrey))
+
+                        val videoID: String = searchContents.getJSONObject(x).getJSONObject("videoRenderer").optString("videoId").toString()
+                        val videoTitle: String = searchContents.getJSONObject(x).getJSONObject("videoRenderer").getJSONObject("title").getJSONArray("runs").getJSONObject(0).optString("text").toString()
+
+                        val videoTitleTextView = TextView(applicationContext)
+                        videoTitleTextView.layoutParams = LinearLayout.LayoutParams(deviceWidth, 156)
+                        videoTitleTextView.text = videoTitle
+                        videoTitleTextView.gravity = Gravity.CENTER_VERTICAL
+                        videoTitleTextView.setTextColor(applicationContext.getColor(R.color.white))
+
+                        val videoButton = View(applicationContext)
+                        videoButton.layoutParams = LinearLayout.LayoutParams(deviceWidth, 156)
+                        videoButton.setOnClickListener {
+                            val playerRequest = extractor.playerRequest(applicationContext, videoID)
+                            val dislikesRequest = extractor.returnYouTubeDislikesRequest(videoID)
+                            val sponsorBlockRequest = extractor.sponsorBlockRequest(videoID)
+
+                            val loader = Loader()
+                            val loaderPlayerInfo = loader.playerInit(playerRequest)
+                            val loaderDislikesInfo = loader.dislikesInit(dislikesRequest)
+
+                            val gson = Gson()
+                            Application.setVideoData(gson.toJson(Data(
+                                loaderPlayerInfo[0].toString(),
+                                loaderPlayerInfo[1].toString(),
+                                sponsorBlockRequest,
+                                loaderPlayerInfo[2].toString(),
+                                loaderPlayerInfo[3].toString(),
+                                loaderPlayerInfo[4].toString(),
+                                loaderPlayerInfo[5].toString(),
+                                loaderPlayerInfo[6].toString().toBoolean(),
+                                loaderDislikesInfo[0].toString(),
+                                loaderDislikesInfo[1].toString()
+                            )))
+
+                            val intent = Intent(this@Search, Player::class.java)
+                            startActivity(intent)
+                        }
+                        searchRelativeView.addView(videoTitleTextView)
+                        searchRelativeView.addView(videoButton)
+
+                        val spaceView = Space(applicationContext)
+                        spaceView.minimumHeight = 4
+                        searchScrollView.addView(spaceView)
+                        searchScrollView.addView(searchRelativeView)
+                    } catch (e: IOException) {
+                        Log.e("IOException", e.toString())
+                    } catch (e: JSONException) {
+                        Log.e("JSONException", e.toString())
+                    }
+                    x += 1
+                }
             }
         }
     }
