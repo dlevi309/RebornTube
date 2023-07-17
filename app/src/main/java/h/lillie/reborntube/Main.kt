83a71fa68f91dd203ad913717d6599e04c78558b
Lillie <91358136+LillieH1000@users.noreply.github.com>
2023-07-16 21:14:01 -0400

    Remove clipboard and sharesheet action check

--- app/src/main/java/h/lillie/reborntube/Main.kt
+++ app/src/main/java/h/lillie/reborntube/Main.kt
@@ -16,12 +16,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Main : AppCompatActivity() {
 
-    private var hasRan = 0
-    private var hasCreated = 0
-
     private var deviceType: Boolean = false
     private var deviceHeight = 0
     private var deviceWidth = 0
+
     @Suppress("Deprecation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,33 +41,12 @@ class Main : AppCompatActivity() {
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
         bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavigationViewListener)
         supportFragmentManager.beginTransaction().replace(R.id.fragments, Fragments(0)).commit()
-
-        hasCreated = 1
     }
 
     override fun onDestroy() {
         super.onDestroy()
         finish()
         exitProcess(0)
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus && hasRan == 0 && hasCreated == 1) {
-            hasRan = 1
-            if (intent.action.equals(Intent.ACTION_SEND)) {
-                if (intent.type != null) {
-                    if (intent.type.toString().startsWith("text/")) {
-                        val receivedText = intent.getStringExtra(Intent.EXTRA_TEXT);
-                        if (receivedText != null) {
-                            getInfo(receivedText)
-                            return
-                        }
-                    }
-                }
-            }
-            getInfo(null)
-        }
     }
 
     @Suppress("Deprecation")
@@ -87,7 +64,6 @@ class Main : AppCompatActivity() {
                 startActivity(intent)
             }
             R.id.action_settings -> {
-                getInfo("https://youtu.be/kSOEbBTvU7Q")
             }
         }
         true
@@ -115,46 +91,5 @@ class Main : AppCompatActivity() {
         }
         supportFragmentManager.beginTransaction().replace(R.id.fragments, selectedFragment).commit()
         true
-    }
-
-    private fun getInfo(text: String?) {
-        var info = String()
-        if (text == null) {
-            val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            info = clipboardManager.primaryClip?.getItemAt(0)?.text.toString()
-        } else {
-            info = text
-        }
-        val youtubeRegex = Regex("^.*(?:(?:youtu\\.be\\/|v\\/|vi\\/|u\\/\\w\\/|embed\\/|shorts\\/|live\\/)|(?:(?:watch)?\\?v(?:i)?=|\\&v(?:i)?=))([^#\\&\\?]*).*")
-        val check = youtubeRegex.containsMatchIn(info)
-        if (check) {
-            val result = youtubeRegex.findAll(info).map { it.groupValues[1] }.joinToString()
-
-            val extractor = Extractor()
-            val playerRequest = extractor.playerRequest(applicationContext, result)
-            val dislikesRequest = extractor.returnYouTubeDislikesRequest(result)
-            val sponsorBlockRequest = extractor.sponsorBlockRequest(result)
-
-            val loader = Loader()
-            val loaderPlayerInfo = loader.playerInit(playerRequest)
-            val loaderDislikesInfo = loader.dislikesInit(dislikesRequest)
-
-            val gson = Gson()
-            Application.setVideoData(gson.toJson(Data(
-                loaderPlayerInfo[0].toString(),
-                loaderPlayerInfo[1].toString(),
-                sponsorBlockRequest,
-                loaderPlayerInfo[2].toString(),
-                loaderPlayerInfo[3].toString(),
-                loaderPlayerInfo[4].toString(),
-                loaderPlayerInfo[5].toString(),
-                loaderPlayerInfo[6].toString().toBoolean(),
-                loaderDislikesInfo[0].toString(),
-                loaderDislikesInfo[1].toString()
-            )))
-
-            val intent = Intent(this@Main, Player::class.java)
-            startActivity(intent)
-        }
     }
 }
